60c2985ae3cd8708d6d12d13b962b845a66ff931
BrosLunas <pablo.luna.perez.008@gmail.com>
2024-06-26 13:28:55 +0200

    add splash and logo

--- broslunas/capacitor.config.ts
+++ broslunas/capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.broslunas.Broslunas',
   appName: 'broslunas',
   webDir: 'www'
 };
